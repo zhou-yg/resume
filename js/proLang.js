3cb2404164976a491ad563be4479f04eca376e7b
Joy Youg <zhou.y.g890@gmail.com>
2013-08-02 14:59:33 +0800

    complete event on select frame

--- js/proLang.js
+++ js/proLang.js
@@ -5,6 +5,34 @@ var onSelectFrame = function(){
 	
 	var idArr = new Array();
 	
+	var EH = function(){
+		
+		var oncrutObjId = "select-as";
+		
+		return{
+			init:function(){
+				var obj = Im.getObj(oncrutObjId);
+				obj.style.background = "#06A7FF";
+				obj.style.color = "#ffffff";
+			}
+			,
+			onchangeColor:function(id){
+				if(id != oncrutObjId){
+					var obj = Im.getObj(id);
+					obj.style.background = "#06A7FF";
+					obj.style.color = "#ffffff";
+					this.reset(oncrutObjId);
+					oncrutObjId = id;
+				}
+			}
+			,
+			reset:function(id){
+					var obj = Im.getObj(id);
+					obj.style.background = "#ffffff";
+					obj.style.color = "#000000";
+			}
+		}
+	}();
 	return{
 		init:function(arr){
 			idArr = arr;
@@ -14,7 +42,12 @@ var onSelectFrame = function(){
 		}
 		,
 		setEvent:function(){
-			
+			EH.init();
+			for(var i=0;i<idArr.length;i++){
+				var frameObj = Im.getObj(idArr[i]);
+				frameObj.onmouseover = function(){EH.onchangeColor(this.id)};
+			}
 		}
 	}
 }();
+onSelectFrame.init(idArr);
