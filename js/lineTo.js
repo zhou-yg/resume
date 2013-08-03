62bf8ff426728e6cbf8bbc41aed9a92b2ebf4592
Joy Youg <zhou.y.g890@gmail.com>
2013-08-03 20:30:15 +0800

    add transition effect on the frames

---
+++ js/lineTo.js
@@ -0,0 +1,5 @@
+var lineTo = function(){
+	
+	
+	
+}();
--- js/proLang.js
+++ js/proLang.js
@@ -1,16 +1,14 @@
-var langArr = ["Flash AS","Java","PHP","HTML+CSS+JavaScript","C/C++","Android","mysql","sqlite","sqlserver"];
-var idArr = ["select-as","select-java","select-php","select-js","select-c"];
-
 var onSelectFrame = function(){
 	
 	var idArr = new Array();
 	
 	var EH = function(){
 		
-		var oncrutObjId = "select-as";
+		var oncrutObjId;
 		
 		return{
 			init:function(){
+				oncrutObjId = idArr[0];
 				var obj = Im.getObj(oncrutObjId);
 				obj.style.background = "#06A7FF";
 				obj.style.color = "#ffffff";
@@ -36,18 +34,30 @@ var onSelectFrame = function(){
 	return{
 		init:function(arr){
 			idArr = arr;
-			this.setEvent();
 
+			EH.init();
+ 
+ 			this.setEvent();
 			return idArr;
 		}
 		,
 		setEvent:function(){
-			EH.init();
 			for(var i=0;i<idArr.length;i++){
 				var frameObj = Im.getObj(idArr[i]);
 				frameObj.onmouseover = function(){EH.onchangeColor(this.id)};
 			}
 		}
 	}
-}();
-onSelectFrame.init(idArr);
+};
+var langArr = ["Flash AS","Java","PHP","HTML+CSS+JavaScript","C/C++","Android","mysql","sqlite","sqlserver"];
+var idlangArr = ["select-as","select-java","select-php","select-js","select-c"];
+var idtitleArr = ["type0","type1","type2"];
+
+/////////////////////////////////
+var onTitleFrame = onSelectFrame;
+
+onTitleFrame = onTitleFrame();
+onSelectFrame = onSelectFrame();
+
+onTitleFrame.init(idtitleArr);
+onSelectFrame.init(idlangArr);
